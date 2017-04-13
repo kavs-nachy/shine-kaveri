cdb068d5d0767ad912a88cb24dfb4178f0543100
kavs-nachy <kaveri.nachammaiece@ksrce.ac.in>
2017-04-13 15:32:01 +0530

    jjkkl
    ggg

---
+++ add.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class sum {
+public static void main (String args[]) {
+inii1=Integer.ParseInt(args[0]);
+inti2=Integer.ParseInt(args[1]);
+int sum=i1+i2;
+System.out.println(sum); }
+}
